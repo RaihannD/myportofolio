73f0f8ca9167a5b802ea3d1b5a644876920422a3
RaihannD <raihandaffaa304@gmail.com>
2026-09-09 14:25:11 +0700

    feat: implement MVT structure and experience page

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Raihan",
+        "fullname": "Raihan Daffa Aprilianda",
+        "npm": "2506620021",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia currently on my 3rd semester."
+            "Wishing to be good at programming someday."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Raihan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
